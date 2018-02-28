cf7e17669f3d94f6082464d0bc2f48d4f950ea30
Justin Kimn <djkimchee@gmail.com>
2018-02-27 20:03:26 -0800

    fixed up footer position

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -15,47 +15,6 @@ const Main = (props) => {
 					<div className="jumbotron">
 						<img className='main-logo' src={logo} alt="logo" />
 					</div>
-					{/*<div className="row">
-						<div>
-							<h2>Black Label Gourmet’s Philosophy: Ingredient First!</h2>
-							<p>Black Label Gourmet carefully selected each ingredient in order to produce a wholesome and great tasting seaweed snack.
-								<br/><br/>We only use 2 colors in our package - black and white.
-								<br/><br/>Black represents seaweed and white represents salt. This demonstrates our principle to exclude everything else from our product besides seaweed, salt and olive oil.</p>
-							<iframe classname="iframe" src="https://docs.google.com/gview?url=https://docs.google.com/presentation/d/1YidaytEcGZWmQlAADBD_8FpCNtUtzUVc-Sj1dAE6M2c/edit#slide=id.p9&embedded=true" frameborder="0"></iframe>
-							<p><a className="btn btn-default" href="#" role="button">View details »</a></p>
-						</div>
-						<div className="col-xs-6 col-lg-4">
-							<h2>Source of Black Label Gourmet: Seocheon Region</h2>
-							<p>The optimal environment to produce high quality seaweed</p>
-							<p><a className="btn btn-default" href="#" role="button">View details »</a></p>
-						</div>
-						<div className="col-xs-6 col-lg-4">
-							<h2>Trustworthy Production Process</h2>
-							<p>A master craftsman, who has devoted the past 25 years to research and rigorous work into implementing the original flavor of healthy seaweed, is fully hands-on in maintaining the high quality of Black Label Gourmet Organic Roasted Seaweed Snack</p>
-							<p><a className="btn btn-default" href="#" role="button">View details »</a></p>
-						</div>
-						<div className="col-xs-6 col-lg-4">
-							<h2>Product Information</h2>
-							<p>∙ Product Name :  Black Label Gourmet Organic Roasted Seaweed Snack
-								<br/>∙ Distributed by :  RICOLOCO INC.
-								<br/>∙ Country of Origin :  Product of Korea 
-								<br/>∙ Net WT :  4.5g (0.16 oz) X 16 Packs
-								<br/>∙ Ingredients :  Seaweed (organic),   Olive Oil (organic), Sun-dried Sea Salt (organic)
-								<br/>∙ Quality of Seaweed : Prime (highest rating)
-								<br/>∙ Certification :  USDA, FDA, HACCP</p>
-							<p><a className="btn btn-default" href="#" role="button">View details »</a></p>
-						</div>
-						<div className="col-xs-6 col-lg-4">
-							<h2>Heading</h2>
-							<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-							<p><a className="btn btn-default" href="#" role="button">View details »</a></p>
-						</div>
-						<div className="col-xs-6 col-lg-4">
-							<h2>Heading</h2>
-							<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-							<p><a className="btn btn-default" href="#" role="button">View details »</a></p>
-						</div>
-					</div>*/}
 				</div>
 
 				<div className="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
@@ -83,10 +42,16 @@ const Main = (props) => {
 					</div>
 				</div>
 			</div>
-
-			<hr />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
 
 			<footer>
+				<hr />
 				<p>For further information, please contact moon@blgourmet.com</p>
 			</footer>
 
